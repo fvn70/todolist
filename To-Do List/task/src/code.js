a89957329bbac07440ce5f197ac5059e28b54742
fvn70 <fvn_reg@mail.ru>
2022-04-03 23:25:06 +0700

    Stage 4/4: Keep in memory

--- To-Do List/task/src/code.js
+++ To-Do List/task/src/code.js
@@ -1,3 +1,6 @@
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+loadData();
+
 let chk = document.getElementsByClassName('check');
 let span = document.getElementsByClassName('task');
 for (let i = 0; i < chk.length; i++) {
@@ -7,26 +10,46 @@ for (let i = 0; i < chk.length; i++) {
         } else {
             span[i].style.textDecoration = 'none';
         }
+        saveData();
     })
+}
+
+function saveData() {
+    taskList = []
+    for (let i = 0; i < span.length; i++) {
+        let task = { 'text': span[i].innerHTML, 'chk': chk[i].checked }
+        taskList[i] = task;
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+function loadData() {
+    for (i in taskList) {
+        let task = taskList[i];
+        crtTask(task.text, task.chk);
+    }
 }
 
 function delTask(obj) {
     // alert('Delete task ' + obj.parentNode.nodeName);
     obj.parentNode.remove();
+    saveData();
 }
 
 function addTask() {
     let task = document.getElementById('input-task');
     crtTask(task.value);
     task.value = "";
+    saveData();
 }
 
-function crtTask(text) {
+function crtTask(text, chk) {
     let liE = document.createElement('li');
     document.getElementById('task-list').appendChild(liE);
     let inputE = document.createElement('input');
     inputE.setAttribute('class', 'check');
     inputE.setAttribute('type', 'checkbox');
+    inputE.checked = chk;
     liE.appendChild(inputE);
     let spanE = document.createElement('span');
     spanE.setAttribute('class', "task");
